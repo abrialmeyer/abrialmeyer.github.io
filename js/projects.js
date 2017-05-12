bc6c02446b65630359121fcf94a23366291031a4
ammeyer <ammeyer@wesleyan.edu>
2017-05-12 15:31:47 -0400

    commit

---
+++ js/projects.js
@@ -0,0 +1,90 @@
+var projects = { 
+	types : [
+		"food", "outdoors", "art", "academic"
+	],
+	about : {
+		text : [ "Hey!", "I'm an environmentalist, artist, educator, cook, maker based in Brooklyn."],
+	},
+	main : [
+		{
+			title : "Rooftop garden",
+			type : "food",
+			description : ["In progress project as part of fellowship at American Museum of Natural History. To be completed in June 2017.", "The Discovery Room offers families, and especially children ages 5-12, an interactive gateway to the wonders of the Museum and a hands-on, behind-the scenes look at its science. Every major field of Museum science and research, from anthropology to zoology, is represented. Children, accompanied by adults, can explore an array of artifacts and specimens, puzzles, and scientific challenges. Activities for older visitors (aged 8+) are located on the upper level.", "The current earthquake exhibit lets visitors track real-time earthquakes on a three-drum seismograph as well as mapping earthquake locations on a digital map of the world.", "Unfortunately, the seismograph no longer works and interest in the exhibit has waned.",  "By redesigning the exhibit as my Helen Fellowship project, I hope to bring new interaction and reinvigorate excitement around earthquake education and awareness.", "For documentation about the design and development process, please see the case study."],
+			haslink : false,
+			links : [""],
+			images : ["images/earthquake/earthquake1.png", "images/earthquake/earthquake2.png"],
+			vimeo : [],
+			youtube : []
+		},
+		{
+			title : "Fermentation",
+			type : "food",
+			description : ["Round is a tool to help users cultivate a well-rounded point of view. A Chrome extension that tracks online news consumption habits and provides easy to read quantitative feedback as an impetus for self-change.", "Thesis Abstract: The propagation of content sharing on social media has drastically changed the way that we experience the news. Instead of going directly to complete and curated primary sources we are fed through secondary sharing and predictive content algorithms. While this is convenient for casual online browsing, the news that is delivered in this system tends to be highly trend-driven, extremely biased, and narrow in topical scope. To address this issue of unbalanced news consumption and general lack of erudite knowledge, I created a browser extension called Round.", "Round began as a project called 'Segue,' a browsing platform for news media, where a database of articles would be connected through hyperlink style keyword connections, allowing users to switch from one article through semantic connections.", "Through several rounds of user testing and research, Segue transitioned into Round, instead of providing a set browsing experience I allowed the user to browse the news as their existing habits dictated. The tool focused on providing feedback on these habits. At this point, the project had completely stepped away from the idea of alternative browsing and new content discovery and focused instead on helping users become well rounded news consumers through reflection on habits. It became more about tracking what and how the user reads and giving feedback through data visualization. This focus on the quantified self as a means for change stems from a desire to prompt change from within instead of forcing it. By letting users recognize their deficiencies on their own they can become their own means for change as desired.", "For more information about the development process of Round, please see the case study."],
+			haslink : true,
+			links : [""],
+			images : ["images/round/round01.jpg", "images/round/round02.png", "images/round/round05.png", "images/round/round06.png", "images/round/round07.png"],
+			vimeo : [],
+			youtube : []
+		},
+		{
+			title : "Do It",
+			type : "animation",
+			description : ["Do It is a piece aimed at increasing the availability of comprehensive sexual education for teens by raising awareness about a significant lack of knowledge in the demographic.", "Sex education is stereotypically stodgy and limited. Do It aims to bring humor to education through sexual colloquialisms and adorable bean-shaped characters.", "In order to create this animation, I worked with sexual educators and researched high school sex-ed curriculum. These resources contributed the facts that I used in the animation.", "All art assets and sound design were self-made."],
+			haslink : false,
+			links : [""],
+			images : ["images/doit/doit01.png", "images/doit/doit02.png", "images/doit/doit03.png"],
+			vimeo : ["https://player.vimeo.com/video/128102510?byline=0&portrait=0"],
+			youtube : []
+		},
+		{
+			title : "Amazon Alicia",
+			type : "design",
+			description : ["Alicia, the off-brand search tool for Amazon Alexa.", "Created for the Stupid Shit That No One Needs and Terrible Ideas Hackathon in NYC on February 25, 2017", "Amazon Alicia began as the idea that it would be funny if Siri was a little deaf. After researching the developer tools for Siri we learned that they were way too opaque and locked down to make something like this, so we transitioned into developing an Alexa Skill.", "The goal of Amazon Alicia was to make a search skill that would return results that sounded similar but weren't quite what you asked for.", "Amazon Alicia was accomplished using the rhyming API Datamuse in combination with the Wikipedia API."],
+			haslink : true,
+			links : [""],
+			images : [],
+			vimeo : [],
+			youtube : ["https://www.youtube.com/embed/kd1gjUt7gI0"]
+		},
+		{
+			title : "Head Haiku",
+			type : "design",
+			description : ["Created after a long day filled with trolls on the internet, Head Haiku is a tool for you release frustration and spend time meditating over poetry generated from your velocity at which your face hits the keyboard."],
+			haslink : true,
+			links : [""],
+			images : ["images/headhaiku/headhaiku.gif"],
+			vimeo : [],
+			youtube : []
+		},
+		{
+			title : "Creatures",
+			type : "interactive",
+			description : ["Creatures is an interactive installation that simulates the appearance of life and explores individual and emergent group behaviors in the context of an artists interpretation of a coral reef."],
+			haslink : false,
+			links : [""],
+			images : [],
+			vimeo : ["https://player.vimeo.com/video/128754679?title=0&byline=0&portrait=0"],
+			youtube : []
+		},
+		{
+			title : "Segue",
+			type : "design",
+			description : ["Segue is an experimental tool for alternative browsing and content discovery.", "Articles from the New York Times are discovered via user generated keywords, and connections between articles are found via a conceptual semantic network.", "For example, a search for “food” brings up an article on fajitas. This article has a keyword of “meat”, which is semantically connected to the idea of a “butcher shop”. Searching for this brings up an article that features an interview with a refugee who used to be a butcher."],
+			haslink : false,
+			links : [""],
+			images : ["images/segue/segue1.png", "images/segue/segue2.png", "images/segue/segue3.png"],
+			vimeo : [],
+			youtube : []
+		},
+		{
+			title : "Sunglasses",
+			type : "interactive",
+			description : ["Taking a fresh look at the needs of brightly colored party-size sunglasses wearers. Don't those lenses get in the way at the most inconvenient moments? Well, these Sunglasses will make sure to open up and free your vision when you need them to."],
+			haslink : false,
+			links : [""],
+			images : ["images/sunglasses/sunglasses.gif"],
+			vimeo : [],
+			youtube : ["https://www.youtube.com/embed/0c87tY9SYs4"]
+		}
+	]
+}
