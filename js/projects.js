3a961061f551077a9ca456e8e2d36c28f22396f2
abrialmeyer <abswa@salterpoint.org>
2018-09-29 15:27:25 -0400

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -9,7 +9,7 @@ var projects = {
 		{
 			title : "Farming/Gardening",
 			type : "food",
-			description : [""]
+			description : [""],
 			links : [""],
 			images : ["images/earthquake/earthquake1.png", "images/earthquake/earthquake2.png"],
 			vimeo : [],
@@ -18,7 +18,7 @@ var projects = {
 		{
 			title : "Cooking Projects",
 			type : "food",
-			description : [""]
+			description : [""],
 			haslink : false,
 			links : [""],
 			images : ["images/round/round01.jpg", "images/round/round02.png", "images/round/round05.png", "images/round/round06.png", "images/round/round07.png"],
@@ -36,29 +36,29 @@ var projects = {
 			youtube : []
 		},
 		{
-			title : "Amazon Alicia",
-			type : "design",
-			description : ["Alicia, the off-brand search tool for Amazon Alexa.", "Created for the Stupid Shit That No One Needs and Terrible Ideas Hackathon in NYC on February 25, 2017", "Amazon Alicia began as the idea that it would be funny if Siri was a little deaf. After researching the developer tools for Siri we learned that they were way too opaque and locked down to make something like this, so we transitioned into developing an Alexa Skill.", "The goal of Amazon Alicia was to make a search skill that would return results that sounded similar but weren't quite what you asked for.", "Amazon Alicia was accomplished using the rhyming API Datamuse in combination with the Wikipedia API."],
-			haslink : true,
+			title : "Art",
+			type : "art",
+			description : [""],
+			haslink : false,
 			links : [""],
 			images : [],
 			vimeo : [],
-			youtube : ["https://www.youtube.com/embed/kd1gjUt7gI0"]
+			youtube : []
 		},
 		{
-			title : "Head Haiku",
-			type : "design",
-			description : ["Created after a long day filled with trolls on the internet, Head Haiku is a tool for you release frustration and spend time meditating over poetry generated from your velocity at which your face hits the keyboard."],
-			haslink : true,
+			title : "Sustainability",
+			type : "academic",
+			description : [""],
+			haslink : false,
 			links : [""],
 			images : ["images/headhaiku/headhaiku.gif"],
 			vimeo : [],
 			youtube : []
 		},
 		{
-			title : "Creatures",
-			type : "interactive",
-			description : ["Creatures is an interactive installation that simulates the appearance of life and explores individual and emergent group behaviors in the context of an artists interpretation of a coral reef."],
+			title : "Teaching",
+			type : "academic",
+			description : [""],
 			haslink : false,
 			links : [""],
 			images : [],
@@ -66,24 +66,14 @@ var projects = {
 			youtube : []
 		},
 		{
-			title : "Segue",
-			type : "design",
-			description : ["Segue is an experimental tool for alternative browsing and content discovery.", "Articles from the New York Times are discovered via user generated keywords, and connections between articles are found via a conceptual semantic network.", "For example, a search for “food” brings up an article on fajitas. This article has a keyword of “meat”, which is semantically connected to the idea of a “butcher shop”. Searching for this brings up an article that features an interview with a refugee who used to be a butcher."],
+			title : "Senior Essay",
+			type : "academic",
+			description : [""],
 			haslink : false,
 			links : [""],
 			images : ["images/segue/segue1.png", "images/segue/segue2.png", "images/segue/segue3.png"],
 			vimeo : [],
 			youtube : []
 		},
-		{
-			title : "Sunglasses",
-			type : "interactive",
-			description : ["Taking a fresh look at the needs of brightly colored party-size sunglasses wearers. Don't those lenses get in the way at the most inconvenient moments? Well, these Sunglasses will make sure to open up and free your vision when you need them to."],
-			haslink : false,
-			links : [""],
-			images : ["images/sunglasses/sunglasses.gif"],
-			vimeo : [],
-			youtube : ["https://www.youtube.com/embed/0c87tY9SYs4"]
-		}
 	]
 }
